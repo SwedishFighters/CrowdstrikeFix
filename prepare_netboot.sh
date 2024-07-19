63a70e447c353f9261a1d45b315a5fbe65f27bf0
SwedishFighters <swedishfighters+jj@areusecure.se>
2024-07-19 14:31:28 +0200

    Delete prepare_netboot.sh
    New solution, see build.sh

--- prepare_netboot.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-wget https://dl-cdn.alpinelinux.org/alpine/v3.18/releases/x86_64/alpine-minirootfs-3.18.0-x86_64.tar.gz
-mkdir alpine-netboot
-cd alpine-netboot
-tar -xzvf ../alpine-minirootfs-3.18.0-x86_64.tar.gz
-sudo chroot . /bin/ash
-apk update
-apk add dislocker fuse ntfs-3g openldap-clients krb5 bash
-exit
